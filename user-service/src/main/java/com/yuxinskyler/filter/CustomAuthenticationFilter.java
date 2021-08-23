1eb825d085047255ab4eab25ec568a56741e033b
SKewLinez <skewlinezofficial@gmail.com>
2021-08-24 01:15:35 +0930

    Injected dependency for JWT.

--- user-service/src/main/java/com/yuxinskyler/filter/CustomAuthenticationFilter.java
+++ user-service/src/main/java/com/yuxinskyler/filter/CustomAuthenticationFilter.java
@@ -1,7 +1,53 @@
 package com.yuxinskyler.filter;
 
+import java.io.IOException;
+import javax.naming.AuthenticationException;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-    
+@Slf4j
+public class CustomAuthenticationFilter
+  extends UsernamePasswordAuthenticationFilter {
+
+  private final AuthenticationManager authenticationManager;
+
+  public CustomAuthenticationFilter(
+    AuthenticationManager authenticationManager
+  ) {
+    this.authenticationManager = authenticationManager;
+  }
+
+  @Override
+  public Authentication attemptAuthentication(
+    HttpServletRequest request,
+    HttpServletResponse response
+  ) {
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    log.info("Username is: {}", username);
+    log.info("Password is: {}", password);
+    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+      username,
+      password
+    );
+    return authenticationManager.authenticate(authenticationToken);
+  }
+
+  @Override
+  protected void successfulAuthentication(
+    HttpServletRequest request,
+    HttpServletResponse response,
+    FilterChain chain,
+    Authentication authResult
+  )
+    throws IOException, ServletException {
+        //TODO: access tokens and refresh tokens
+    super.successfulAuthentication(request, response, chain, authResult);
+  }
 }
